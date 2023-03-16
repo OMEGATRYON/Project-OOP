fb69a4addf46c498dd3a63bc20dca8ade2974125
OMEGATRYON <tryon.baldwin@student.awesomeinc.org>
2023-03-16 19:41:08 +0000

    added some sort of basic structure

---
+++ src/category.py
@@ -0,0 +1,14 @@
+class category: 
+    def __init__(self, ledger):
+         self.ledger = []
+
+    def withdraw(self):
+        pass
+    def deposit(self):
+        pass
+    def get_balance(self):
+        pass
+    def transfer(self):
+        pass
+    def check_funds(self):
+        pass
