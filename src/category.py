3b9254dbe2e834135f5b3913626fdbbd6ee5bd99
OMEGATRYON <tryon.baldwin@student.awesomeinc.org>
2023-03-17 14:47:14 +0000

    added withdraw so far

--- src/category.py
+++ src/category.py
@@ -1,9 +1,16 @@
 class category: 
     def __init__(self, ledger):
          self.ledger = []
+         self.amount = 0
+         self.description = ""
 
-    def withdraw(self):
-        pass
+    def withdraw(self, monies, description):
+        if monies > self.amount:
+            print("you broke bro")
+        elif monies <= self.amount:
+            print(self.amount)
+        self.ledger.append({'Amount': (transaction), 'Description': description})
+        print(self.ledger)
     def deposit(self):
         pass
     def get_balance(self):
